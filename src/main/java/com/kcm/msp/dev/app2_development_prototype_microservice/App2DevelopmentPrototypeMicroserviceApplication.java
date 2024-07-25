25d9d6a663d3c1928ce03676f6a90d36f1a03461
kannan.c.mohan <kannanmohanklm@gmail.com>
2024-07-25 12:44:46 +0200

    feat: add springboot3 prototype project

---
+++ src/main/java/com/kcm/msp/dev/app2_development_prototype_microservice/App2DevelopmentPrototypeMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.kcm.msp.dev.app2_development_prototype_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App2DevelopmentPrototypeMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App2DevelopmentPrototypeMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kcm/msp/dev/app2_development_prototype_microservice/App2DevelopmentPrototypeMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kcm.msp.dev.app2_development_prototype_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class App2DevelopmentPrototypeMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
