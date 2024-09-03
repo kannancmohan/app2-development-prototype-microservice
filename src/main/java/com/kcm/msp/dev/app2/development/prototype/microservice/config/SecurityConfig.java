35ede2a0f2f4cf14e05604a997210e6ef2b76a6c
kannan.c.mohan <kannanmohanklm@gmail.com>
2024-09-03 17:25:46 +0200

    feat: add test for jwt security check

--- src/main/java/com/kcm/msp/dev/app2/development/prototype/microservice/config/SecurityConfig.java
+++ src/main/java/com/kcm/msp/dev/app2/development/prototype/microservice/config/SecurityConfig.java
@@ -14,7 +14,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -33,7 +32,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity // comment this "To disable springboot-security"
-@EnableMethodSecurity // added to make use of @PreAuthorize and @PostAuthorize
+// @EnableMethodSecurity // added to make use of @PreAuthorize and @PostAuthorize
 @RequiredArgsConstructor
 public class SecurityConfig {
 
